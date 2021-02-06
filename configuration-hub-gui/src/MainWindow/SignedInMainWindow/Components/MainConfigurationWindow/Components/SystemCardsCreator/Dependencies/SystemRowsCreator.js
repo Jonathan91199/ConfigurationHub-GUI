e77f592e00a05296cf7c2211f482e17b54fbe3c9
Jonathan91199 <jonathan.c911@gmail.com>
2021-02-06 14:49:45 +0200

    changing RowCreator to a standAlone Function

--- configuration-hub-gui/src/MainWindow/SignedInMainWindow/Components/MainConfigurationWindow/Components/SystemCardsCreator/Dependencies/SystemRowsCreator.js
+++ configuration-hub-gui/src/MainWindow/SignedInMainWindow/Components/MainConfigurationWindow/Components/SystemCardsCreator/Dependencies/SystemRowsCreator.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 
-const NUM_OF_SYSTEM_IN_ROW = 4
 
-export default function SystemRowCreator(that) {
+export default function SystemRowCreator(that, optionArr, NumberOfRows) {
     let cols = []
-    for (let rowNum = 0; rowNum < Math.floor(((that.props.allSystems.length) / NUM_OF_SYSTEM_IN_ROW)) + 1; rowNum++) {
+    for (let rowNum = 0; rowNum < Math.floor(((optionArr.length) / NumberOfRows)) + 1; rowNum++) {
         cols[rowNum] = []
-        for (let colNum = 0; colNum < NUM_OF_SYSTEM_IN_ROW; colNum++) {
-            let system = that.props.allSystems[(rowNum * NUM_OF_SYSTEM_IN_ROW + colNum)]
-            if (that.props.allSystems[(rowNum * NUM_OF_SYSTEM_IN_ROW + colNum)] !== undefined) {
+        for (let colNum = 0; colNum < NumberOfRows; colNum++) {
+            let system = optionArr[(rowNum * NumberOfRows + colNum)]
+            if (optionArr[(rowNum * NumberOfRows + colNum)] !== undefined) {
                 cols[rowNum][colNum] =
-                    <Col className="SystemColMainDiv" onClick={() => that.handleSystemClick(system.id)}>
-                        <div className="SystemCol" >{system.name}</div>
+                    <Col className="RowCreatorColMainDiv" onClick={() => that.handleClick(system)}>
+                        <div className="RowCreatorCol" >{system.name}</div>
                     </Col>
             }
             else { cols[rowNum][colNum] = <Col className="EmptyCol"></Col> }
@@ -21,12 +20,13 @@ export default function SystemRowCreator(that) {
     let AllSystems = []
     let tempCol = []
     for (let rowNum = 0; rowNum < cols.length; rowNum++) {
-        AllSystems.push(<Row className="SystemRow" id={`RowNum${rowNum}`}>{tempCol}</Row>)
+        AllSystems.push(<Row className="RowCreatorRow" id={`RowNum${rowNum}`}>{tempCol}</Row>)
         for (let colNum = 0; colNum < cols[rowNum].length; colNum++) {
             tempCol.push(cols[rowNum][colNum])
         }
         tempCol = []
 
     }
-    that.setState({ AllSystemsState: AllSystems, PageState: "ChooseSystem" })
+    return AllSystems
+    // that.setState({ AllSystemsState: AllSystems, PageState: "ChooseSystem" })
 }
